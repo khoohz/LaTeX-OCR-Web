8626a52b24deae4f2d5230a004b1a65c3046b26b
khoohz <hz2000.khz@gmail.com>
2024-06-27 15:55:49 +0800

    solving weights path issue

--- streamlit.py
+++ streamlit.py
@@ -3,11 +3,13 @@ from PIL import Image
 import streamlit as st
 import os
 from pix2tex.cli import LatexOCR
-from pix2tex.model.checkpoints import get_latest_checkpoint
 from sympy.parsing.latex import parse_latex
 from sympy.parsing.latex.errors import LaTeXParsingError
 from sympy.solvers import solve
 from sympy import symbols
+import tqdm
+import io
+import requests
 
 if 'latex_code' not in st.session_state:
     st.session_state['latex_code'] = ''
@@ -110,11 +112,44 @@ def calculate():
         print("Error in calculation:", e)
         st.error("Error in calculation")
 
-def download_checkpoints_custom_path():
-    path = './weights'
-    os.makedirs(path, exist_ok=True)
-    get_latest_checkpoint(path=path)
+def get_latest_tag():
+    r = requests.get(url)
+    tag = r.url.split('/')[-1]
+    if tag == 'releases':
+        return 'v0.0.1'
+    return tag
 
+
+def download_as_bytes_with_progress(url: str, name: str = None) -> bytes:
+    # source: https://stackoverflow.com/questions/71459213/requests-tqdm-to-a-variable
+    resp = requests.get(url, stream=True, allow_redirects=True)
+    total = int(resp.headers.get('content-length', 0))
+    bio = io.BytesIO()
+    if name is None:
+        name = url
+    with tqdm.tqdm(
+        desc=name,
+        total=total,
+        unit='b',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for chunk in resp.iter_content(chunk_size=65536):
+            bar.update(len(chunk))
+            bio.write(chunk)
+    return bio.getvalue()
+
+
+def download_checkpoints_custom_path():
+    tag = get_latest_tag()
+    path = './weights'
+    print('download weights', tag, 'to path', path)
+    weights = 'https://github.com/lukas-blecher/LaTeX-OCR/releases/download/%s/weights.pth' % tag
+    resizer = 'https://github.com/lukas-blecher/LaTeX-OCR/releases/download/%s/image_resizer.pth' % tag
+    for url, name in zip([weights, resizer], ['weights.pth', 'image_resizer.pth']):
+        file = download_as_bytes_with_progress(url, name)
+        open(os.path.join(path, name), "wb").write(file)
+        
 class CustomLatexOCR(LatexOCR):
     def __init__(self):
         download_checkpoints_custom_path()
